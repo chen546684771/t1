58d27084e5b24dc0bb97ebf912de726afbf2812c
chen546684771 <546684771@qq.com>
2020-11-03 16:45:29 +0800

    qqq

--- src/main/java/com/chen/sad/App.java
+++ src/main/java/com/chen/sad/App.java
@@ -1,13 +1,13 @@
 package com.chen.sad;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+   public static void main(String[] args) throws Exception {
+	SpringApplication.run(App.class, args);
+}
+
 }
